9a54cf1cffb487c7a46e994fa1e65a3809366d8d
Nathan Koga <103086476+nathankoga@users.noreply.github.com>
2022-12-13 18:43:53 -0800

    Add files via upload

--- tic_tests.py
+++ tic_tests.py
@@ -1,3 +1,88 @@
-import doctest
+import unittest
 import tictactoe
 
+
+class T0_board_checks(unittest.TestCase):
+
+    def test_horizontal_one(self):
+        print("top row win condition")
+        board = tictactoe.Board()
+        board.board = ["X", "X", "X",
+                       "", "", "",
+                       "", "", ""]
+        self.assertEqual(board.win_check(board.board), "X")
+
+    def test_horizontal_two(self):
+        print("middle row win condition")
+        board = tictactoe.Board()
+        board.board = ["", "", "",
+                       "X", "X", "X",
+                       "", "", ""]
+        self.assertEqual(board.win_check(board.board), "X")
+
+    def test_horizontal_three(self):
+        print("bottom row win condition")
+        board = tictactoe.Board()
+        board.board = ["", "", "",
+                       "", "", "",
+                       "X", "X", "X"]
+        self.assertEqual(board.win_check(board.board), "X")
+
+    def test_vertical_one(self):
+        print("first column win condition")
+        board = tictactoe.Board()
+        board.board = ["X", "", "",
+                       "X", "", "",
+                       "X", "", ""]
+        self.assertEqual(board.win_check(board.board), "X")
+
+    def test_vertical_two(self):
+        print("second column win condition")
+        board = tictactoe.Board()
+        board.board = ["", "O", "",
+                       "", "O", "",
+                       "", "O", ""]
+        self.assertEqual(board.win_check(board.board), "O")
+
+    def test_vertical_three(self):
+        print("last column win condition")
+        board = tictactoe.Board()
+        board.board = ["", "", "O",
+                       "", "", "O",
+                       "", "", "O"]
+        self.assertEqual(board.win_check(board.board), "O")
+
+    def test_back_slash(self):
+        print("second column win condition")
+        board = tictactoe.Board()
+        board.board = ["O", "", "",
+                       "", "O", "",
+                       "", "", "O"]
+        self.assertEqual(board.win_check(board.board), "O")
+
+    def test_forward_slash(self):
+        print("last column win condition")
+        board = tictactoe.Board()
+        board.board = ["", "", "O",
+                       "", "O", "",
+                       "O", "", ""]
+        self.assertEqual(board.win_check(board.board), "O")
+
+    def test_fresh_board(self):
+        print("fresh board")
+        board = tictactoe.Board()
+
+        self.assertEqual(board.win_check(board.board), None)
+
+    def test_full_board(self):
+        print("full board")
+        board = tictactoe.Board()
+        board.board = ["X", "O", "O",
+                       "O", "X", "X",
+                       "O", "X", "O"]
+
+        self.assertEqual(board.win_check(board.board), None)
+
+
+if __name__ == "__main__":
+    unittest.main()
--- tictactoe.py
+++ tictactoe.py
@@ -2,7 +2,6 @@ ROWS = 3
 COLS = 3
 MAX = 9  # Number of maximum moves
 
-
 class Board:
     def __init__(self):
         self.turn = 0
@@ -29,33 +28,35 @@ class Board:
         else:
             print('invalid move!')
 
-    def win_check(self):
-        for item in range(len(self.board)):
-            if self.board[item]:  # if board index is not empty
+    def win_check(self, board):  # has to be generalized, so that logic can be used for potential boards for minimax
+        for item in range(len(board)):
+            if board[item]:  # if board index is not empty
                 if item % 3 == 0:  # item is in left column, check for horizontal win
-                    if self.board[item] == self.board[item+1] and self.board[item] == self.board[item+2]:
-                        self.status = True
-                        return
+                    if board[item] == board[item+1] and board[item] == board[item+2]:
+                        return board[item]  # we return board[item] so that we know which player wins
 
                 if item < 3:  # item is in first column, check for horizontal win
-                    if self.board[item] == self.board[item+3] and self.board[item] == self.board[item+6]:
-
-                        return True
+                    if board[item] == board[item+3] and board[item] == board[item+6]:
+                        return board[item]
 
                 if item == 0:  # item is top left corner, check for diagonal win
-                    if self.board[item] == self.board[item+4] and self.board[item] == self.board[item+8]:
-                        self.status = True
-                        return
+                    if board[item] == board[item+4] and board[item] == board[item+8]:
+                        return board[item]
 
                 if item == 2:  # item is top right corner, check for diagonal win
-                    if self.board[item] == self.board[item+4] and self.board[item] == self.board[item+6]:
-                        self.status = True
-                        return
-            else:  # if none of these found a win
-                return False
+                    if board[item] == board[item+2] and board[item] == board[item+4]:
+                        return board[item]
 
-    def score(self):  # returns an int representing the respective value a move has
-        pass
+        else:  # if none of these found a win
+            return None
+
+    def score(self, board, player):  # returns an int representing the respective value a potential board has
+        if self.win_check(board) == player:
+            return 10  # 10 points for the current player, as there is a win
+        elif self.win_check(board):
+            return -10  # win_check resulted in a win that didn't match the player, therefore, other player wins
+        else:
+            return 0  # no win
 
 
 def main():
@@ -65,7 +66,9 @@ def main():
         player = (board.turn % 2) + 1  # player 1 or player 2's turn
         spot = int(input(f"Player {player}, Make a move (Input a number 0~8): "))
         board.move(player, spot)
-        board.win_check()
+
+        if board.win_check(board.board):
+            board.status = True
 
     # board.move(1, 0)
 
